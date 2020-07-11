253d3bb4e6b4000ef47d736f0957f28c0764498a
Janis Zarzeckis <janis.zarzeckis@ui.com>
2020-07-11 23:10:48 +0300

    Load stage descriptions from firebase

--- src/app/stage/stage.component.ts
+++ src/app/stage/stage.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, pluck, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { map, switchMap, distinctUntilChanged, filter, share, pluck, takeUntil, withLatestFrom } from 'rxjs/operators';
 import { StageService } from '../stage.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { stages } from '../stages.mock';
 import { environment } from 'src/environments/environment';
 import { GoogleMap } from '@googlemaps/map-loader';
 import { Stage } from './stage.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { firestore } from 'firebase/app';
+import { MapLoaderOptions } from '@googlemaps/map-loader/dist/map-loader';
 
 const mapLoader = new GoogleMap();
+const mapLoaderOptions: MapLoaderOptions = {
+  apiKey: environment.googlemap,
+  divId: 'google_map',
+  append: false, // Appends to divId. Set to false to init in divId.
+  mapOptions: {
+    center: {
+      lat: 56.649196,
+      lng: 25.350384
+    },
+    zoom: 8
+  },
+  apiOptions: {
+    version: 'weekly',
+    libraries: ['places']
+  }
+};
 
 @Component({
   selector: 'app-stage',
@@ -22,17 +39,18 @@ const mapLoader = new GoogleMap();
       <a routerLink="./">info</a> 
       <a [routerLink]="['participants']">participants</a>
     </div>
-    <h1>{{title | async}}</h1>
+    <h1>{{title$ | async}}</h1>
     <router-outlet style="display: none"></router-outlet>
     <div *ngIf="!isParticipantVisible">
-      <div class="post" [innerHtml]="description | async"> </div>
+      <div class="post" [innerHtml]="description$ | async"></div>
 
       <div class="details">
         <div id="google_map"></div>
-        <div class="info">
-          <div>Cena: {{stage.price}}</div>
-          <div>Datums: {{stage.date}}</div>
+        <div class="info" *ngIf="stage$ | async as stage">
+          <div>Cena: {{stage.fee}}</div>
+          <div>Datums: {{formatDate(stage.date)}}</div>
           <div>Vieta: {{stage.place}}</div>
+          <div>{{stage.description}}</div>
         </div>
       </div>
     </div>
@@ -44,8 +62,25 @@ const mapLoader = new GoogleMap();
 export class StageComponent implements OnInit, OnDestroy {
   private stage$ = this.activeRoute.params.pipe(
     distinctUntilChanged((a, b) => a.year === b.year && a.id === b.id),
-    switchMap(({ year, id }) => this.afs.doc(`years/${year}/stages/${id}`).valueChanges())
+    switchMap(({ year, id }) =>
+      this.afs.doc<Stage>(`years/${year}/stages/${id}`).valueChanges().pipe(
+        filter((s): s is Stage => s !== undefined),
+        map(s => ({ ...s, id }))
+      )
+    )
   )
+
+  private stagePost$ = this.stage$.pipe(
+    switchMap(stage => this.stageService.getStageDescription(stage.id)),
+    share()
+  )
+
+  description$ = this.stagePost$.pipe(
+    pluck('html'),
+    map(html => this.sanitizer.bypassSecurityTrustHtml(html!))
+  )
+
+  title$ = this.stagePost$.pipe(pluck('title'))
 
   constructor(private activeRoute: ActivatedRoute,
     private stageService: StageService,
@@ -58,60 +93,37 @@ export class StageComponent implements OnInit, OnDestroy {
   title?: Promise<string>;
   stage?: Stage;
 
-  map?: google.maps.Map
+  map = mapLoader.initMap(mapLoaderOptions)
+  marker = this.map.then(map => new google.maps.Marker({
+    map,
+    draggable: false,
+    animation: google.maps.Animation.DROP
+  }))
 
-  async initMap() {
-    const mapOptions = {
-      center: {
-        lat: 56.649196,
-        lng: 25.350384
-      },
-      zoom: 8
-    }
+  unsubscribe$ = new Subject<void>()
 
-    const apiOptions = {
-      version: 'weekly',
-      libraries: ['places']
-    }
-
-    const mapLoaderOptions = {
-      apiKey: environment.googlemap,
-      divId: 'google_map',
-      append: false, // Appends to divId. Set to false to init in divId.
-      mapOptions: mapOptions,
-      apiOptions: apiOptions
-    };
-
-    // Load the map
-    const googleMap = await mapLoader.initMap(mapLoaderOptions);
-
-    new google.maps.Marker({
-      map: googleMap,
-      draggable: false,
-      animation: google.maps.Animation.DROP,
-      position: mapOptions.center
+  ngOnInit() {
+    this.activeRoute.url.subscribe(x => {
+      console.log("url", x)
+    })
+    this.stage$.pipe(
+      takeUntil(this.unsubscribe$),
+      withLatestFrom(this.map, this.marker),
+    ).subscribe(([{ location: { latitude, longitude } }, map, marker]) => {
+      // Whenever stage location changes - pan map to it, and update marker position
+      const latLng: google.maps.LatLngLiteral = { lat: latitude, lng: longitude }
+      map.panTo(latLng);
+      marker.setPosition(latLng);
+      marker.setAnimation(google.maps.Animation.DROP);
     });
-  }
-  async ngOnInit() {
-    const pathParams = this.activeRoute.snapshot.params;
-    this.id = (Object.keys(pathParams).length) ? pathParams.id : '1';
-    const stagePost = this.stageService.getStageDescription(this.id!);
-
-    this.description = stagePost
-      .then(res => res.html)
-      .then(res => this.sanitizer.bypassSecurityTrustHtml(res!));
-
-    this.title = stagePost
-      .then(res => res.title!)
-
-    this.stage = stages[0];
-    await this.initMap()
   }
 
   ngOnDestroy() {
-    if (this.map) {
-      this.map.unbindAll()
-    }
+    this.map.then(map => {
+      map.unbindAll();
+    });
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   formatDate(date: firestore.Timestamp): string {
