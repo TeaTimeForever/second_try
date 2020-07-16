3fc75870cdb545796967d83e6c28337019f88847
Karina Zaržecka <karina.zarzecka@citadele.lv>
2020-07-16 13:36:00 +0300

    Remove mock

--- src/app/stages.mock.ts
+++ src/app/stages.mock.ts
@@ -1,33 +1,4 @@
 import { Stage } from "./stage/stage.model";
 import "firebase/firestore";
-import { firestore } from "firebase/app";
 
-export const stages: Stage[] = [
-  {
-    nr: 1,
-    location: new firestore.GeoPoint(53.1, 42.7),
-    place: "Blakus lidlaukam",
-    date: new firestore.Timestamp(1594410104, 0),
-    fee: "35 eur",
-    status: "finished",
-    description: "",
-  },
-  {
-    nr: 2,
-    location: new firestore.GeoPoint(53.1, 42.7),
-    place: "Blakus lidlaukam",
-    date: new firestore.Timestamp(1594410104, 0),
-    fee: "35 eur",
-    status: "ongoing",
-    description: "",
-  },
-  {
-    nr: 3,
-    location: new firestore.GeoPoint(53.1, 42.7),
-    place: "Blakus lidlaukam",
-    date: new firestore.Timestamp(1594410104, 0),
-    fee: "35 eur",
-    status: "announced",
-    description: "",
-  },
-];
+export const stages: Stage[] = [];
