2f0b7d16532a9742e52fd9290f85d1351db481df
Karina Zaržecka <karina.zarzecka@citadele.lv>
2020-07-29 22:59:13 +0300

    Task columns generated

--- src/app/result-table/result-table.component.ts
+++ src/app/result-table/result-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Participant, HasId } from '../participants/participant.model';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-result-table',
@@ -13,8 +14,10 @@ import { Participant, HasId } from '../participants/participant.model';
   <th></th>
   <th></th>
   <th></th>
-  <th colspan="3">1. uzdevums</th>
-  <th colspan="3">2. uzdevums</th>
+  <ng-container *ngFor="let taskIndex of tasksIds;">
+    <th colspan="3">{{taskIndex+1}}. uzdevums</th>
+  </ng-container>
+
   <th>Kopvērtējums</th>
 </thead>
 <tr>
@@ -23,13 +26,11 @@ import { Participant, HasId } from '../participants/participant.model';
   <th>Vārds</th>
   <th>Spārna klase</th>
 
-  <th>Punkti</th>
-  <th>Distance (km)</th>
-  <th>Ātrums (km/h)</th>
-
-  <th>Punkti</th>
-  <th>Distance (km)</th>
-  <th>Ātrums (km/h)</th>
+  <ng-container *ngFor="let taskIndex of tasksIds;">
+    <th>Punkti</th>
+    <th>Distance (km)</th>
+    <th>Ātrums (km/h)</th>
+  </ng-container>
 
   <th>Punkti</th>
 
@@ -40,17 +41,18 @@ import { Participant, HasId } from '../participants/participant.model';
     <td>{{p.name}} {{p.surname}}</td>
     <td>{{p.wingClass | uppercase}}</td>
 
-    <td>{{p.tasks[0].score}} {{p.tasks[0].penalty? '*' : ''}}</td>
-    <td>{{p.tasks[0].distance}}</td>
-    <td>{{p.tasks[0].speed}}</td>
+    <ng-container *ngFor="let taskIndex of tasksIds;">
+      <td>{{p.tasks[taskIndex].score}} {{p.tasks[0].penalty? '*' : ''}}</td>
+      <td>{{p.tasks[taskIndex].distance}}</td>
+      <td>{{p.tasks[taskIndex].speed}}</td>
+    </ng-container>
 
-    <td>{{p.tasks[1].score}}</td>
-    <td>{{p.tasks[1].distance}}</td>
-    <td>{{p.tasks[1].speed}}</td>
-    <td>{{p.score}}</td>
+    <td>{{p.score}}</td >
     </tr>
   </table>
-  <span class="penalty-description"><b>*</b> 50% sods saskaņā ar nolikuma 11.4 punktu</span>
+  <span class="penalty-description" *ngIf="penaltyNoteIsVisible$ | async">
+    <b>*</b> 50% sods saskaņā ar nolikuma 11.4 punktu
+  </span>
   `,
   styleUrls: ['./result-table.component.scss']
 })
@@ -59,9 +61,15 @@ export class ResultTableComponent implements OnInit {
   @Input() title?: string;
   @Input() color?: string;
   @Input() participants$?: Observable<Array<Participant & HasId>>;
+  @Input() tasksIds = [0, 1];
+  penaltyNoteIsVisible$?: Observable<boolean>;
+  
   constructor() { }
 
   ngOnInit() {
+    this.penaltyNoteIsVisible$ = this.participants$!.pipe(
+      map(participants => !!participants.find(p => !!p.tasks!.find(t => !!t.penalty))),
+      startWith(false));
   }
 
   trackById(_index: number, { id }: HasId) {
